591d2453588d6e9a2c65eb537038e2840b30122e
alve071211 <alve071211@gmail.com>
2024-09-10 15:59:40 +1200

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,6 @@
+#3rd program
+name = (2*2+2)
+print(name)
+name1 = (2*(2+2))
+print(name1)
+print(name==name1)
---
+++ task4.py
@@ -0,0 +1,5 @@
+#4th program
+name=(float('123.456'))
+name1=(name*10)
+int(name1)
+print(int(name1%10))
